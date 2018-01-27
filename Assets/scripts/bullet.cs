11536c1f8ba41432eded3d19d6bce359059c50d4
Twelvemieles <juandimieles99@gmail.com>
2018-01-27 05:23:19 -0500

    bolas

---
+++ Assets/scripts/bullet.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour {
+    Rigidbody2D rb2d;
+    public float fuerzaLanz = 0.02f ;
+    public GameObject ptoLanzamiento;
+    bool enUso;
+    
+	// Use this for initialization
+	void Start () {
+        transform.position = ptoLanzamiento.transform.position;
+    
+    GetComponent<Rigidbody2D>().AddForce(transform.right * fuerzaLanz);
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
